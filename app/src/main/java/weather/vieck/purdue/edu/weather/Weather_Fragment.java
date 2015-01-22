f8902514c18e0644741d7840d22f2e66afafee42
Michael Vieck <vieck@purdue.edu>
2015-01-21 21:59:30 -0500

    Added More Classes to weather fragment

--- app/src/main/java/weather/vieck/purdue/edu/weather/Weather_Fragment.java
+++ app/src/main/java/weather/vieck/purdue/edu/weather/Weather_Fragment.java
@@ -3,13 +3,20 @@ package weather.vieck.purdue.edu.weather;
 import android.app.Fragment;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import android.os.Handler;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Michael on 1/21/2015.
@@ -48,8 +55,95 @@ public class Weather_Fragment extends Fragment {
 
     private void updateWeatherData(final String city) {
         new Thread() {
-            
+            public void run() {
+                final JSONObject json = FetchJSON.getJSON(getActivity(), city);
+                if (json == null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getActivity(),
+                                    getActivity().getString(R.string.place_not_found),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            renderWeather(json);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+
+    private void renderWeather(JSONObject json) {
+        try {
+            cityField.setText(json.getString("name").toUpperCase(Locale.US) +
+                    ", " +
+                    json.getJSONObject("sys").getString("country"));
+
+            JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = json.getJSONObject("main");
+            detailsField.setText(
+                    details.getString("description").toUpperCase(Locale.US) +
+                            "\n" + "Humidity: " + main.getString("humidity") + "%" +
+                            "\n" + "Pressure: " + main.getString("pressure") + " hPa");
+
+            currentTemperatureField.setText(
+                    String.format("%.2f", main.getDouble("temp")) + " ℃");
+
+            DateFormat df = DateFormat.getDateTimeInstance();
+            String updatedOn = df.format(new Date(json.getLong("dt") * 1000));
+            updatedField.setText("Last update: " + updatedOn);
+
+            setWeatherIcon(details.getInt("id"),
+                    json.getJSONObject("sys").getLong("sunrise") * 1000,
+                    json.getJSONObject("sys").getLong("sunset") * 1000);
+
+        } catch (Exception e) {
+            Log.e("SimpleWeather", "One or more fields not found in the JSON data");
         }
+    }
+
+    private void setWeatherIcon(int actualId, long sunrise, long sunset) {
+        int id = actualId / 100;
+        String icon = "";
+        if (actualId == 800) {
+            long currentTime = new Date().getTime();
+            if (currentTime >= sunrise && currentTime < sunset) {
+                icon = getActivity().getString(R.string.weather_sunny);
+            } else {
+                icon = getActivity().getString(R.string.weather_clear_night);
+            }
+        } else {
+            switch (id) {
+                case 2:
+                    icon = getActivity().getString(R.string.weather_thunder);
+                    break;
+                case 3:
+                    icon = getActivity().getString(R.string.weather_drizzle);
+                    break;
+                case 7:
+                    icon = getActivity().getString(R.string.weather_foggy);
+                    break;
+                case 8:
+                    icon = getActivity().getString(R.string.weather_cloudy);
+                    break;
+                case 6:
+                    icon = getActivity().getString(R.string.weather_snowy);
+                    break;
+                case 5:
+                    icon = getActivity().getString(R.string.weather_rainy);
+                    break;
+            }
+        }
+        weatherIcon.setText(icon);
+    }
+
+    public void changeCity(String city) {
+        updateWeatherData(city);
     }
 
     public Weather_Fragment() {
